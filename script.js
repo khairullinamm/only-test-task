1b6e2f00dcd7c77e67d0d76d8d8c514ab76d2773
Milana <milanakhairullina@gmail.com>
2023-09-05 17:57:46 +0300

    add: put labels on fixed places

---
+++ script.js
@@ -0,0 +1,8 @@
+/*
+let image = document.querySelector(".img-bg");
+let container = document.querySelector(".container");
+console.log(image.height);
+console.log(container.offsetHeight);
+container.setAttribute("style", `height: ${image.height}px`);
+console.log(container.offsetHeight);
+*/
